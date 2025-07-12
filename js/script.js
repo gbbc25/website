f25421aaa18b03e0397d2d2ec396f51e2ff3952e
Joel <104278288+JoelW2003@users.noreply.github.com>
2025-07-12 20:46:26 +0100

    started new website
    navigation, home, service times, and activities started. mobile navigation curtain menu needs fixing.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-function openWebsite(website) {
-    if (website) {
-        window.open(website, "_blank"); // Opens in a new tab
-    } else {
-        console.error("No website URL provided!");
-    }
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
 }
-
-function toggleMobileNav() {
-	  const nav = document.getElementById("mobileNav");
-	  nav.classList.toggle("active");
-	}
